0573e648a1097db5015d20095bd3f03446d7fa15
Erik C. Rutledge <4974496+erikcr@users.noreply.github.com>
2024-02-08 14:03:57 -0500

    Add more custom database types.

--- types/app.types.ts
+++ types/app.types.ts
@@ -2,7 +2,53 @@ import { Tables } from "./database.types";
 import { supabase } from "../utils/supabase";
 import { QueryResult, QueryData, QueryError } from '@supabase/supabase-js'
 
-const questionsWithResponses = supabase
+const questionsWithResponsesQuery = supabase
   .from("v002_questions_stag")
   .select(`*, v002_responses_stag ( id, is_correct )`);
-export type QuestionsWithResponses = QueryData<typeof questionsWithResponses>;
+export type QuestionsWithResponses = QueryData<typeof questionsWithResponsesQuery>;
+
+const teamsWithResponsesQuery = supabase
+  .from("v002_teams_stag")
+  .select(`
+    id,
+    name,
+    v002_responses_stag (
+      id,
+      is_correct,
+      submitted_answer,
+      v002_questions_stag (
+        points
+      )
+    )
+  `);
+export type TeamsWithResponses = QueryData<typeof teamsWithResponsesQuery>;
+
+const teamWithResponsesQuery = supabase
+  .from("v002_teams_stag")
+  .select(`
+    id,
+    name,
+    v002_responses_stag (
+      id,
+      is_correct,
+      submitted_answer,
+      v002_questions_stag (
+        points
+      )
+    )
+  `)
+  .limit(1);
+export type TeamWithResponses = QueryData<typeof teamWithResponsesQuery>[0];
+
+const responseWithQuestionsQuery = supabase
+  .from("v002_responses_stag")
+  .select(`
+    id,
+    is_correct,
+    submitted_answer,
+    v002_questions_stag (
+      points
+    )
+  `)
+  .limit(1);
+export type ResponeWithQuestions = QueryData<typeof responseWithQuestionsQuery>;
